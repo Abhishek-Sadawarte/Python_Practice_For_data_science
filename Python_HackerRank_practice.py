d059f6a81124657b90618626cb3ffdbc74e1bab4
Abhishek-Sadawarte <abhi999.sad@gmail.com>
2023-06-26 01:50:04 +0530

    new code

--- Python_HackerRank_practice.py
+++ Python_HackerRank_practice.py
@@ -25,5 +25,19 @@ for i in range(N):
     elif command[0] == 'print':
         print(output)
 
+# Q2 In this challenge, the user enters a string and a substring. 
+# You have to print the number of times that the substring occurs in the given string. 
+# String traversal will take place from left to right, not from right to left.
+string = "ABCDCDC"
+sub_string = "CDC"
+def count_substring(string, sub_string):
+    count = 0 
+    n = len(string)
+    m = len(sub_string)
+    for i in range(n-m+1):
+        if string[i:i+m] == sub_string:
+            count += 1
+    return count
 
+count_substring(string,sub_string)
 
