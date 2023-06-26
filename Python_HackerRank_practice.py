10074a3add668c853c4755621b6e6989ce6dfa08
Abhishek-Sadawarte <abhi999.sad@gmail.com>
2023-06-27 01:21:03 +0530

    new challenge solved

--- Python_HackerRank_practice.py
+++ Python_HackerRank_practice.py
@@ -55,7 +55,7 @@ def SwapCase(s):
             output += i.lower()
     return output
 
-'''Q2 add two string one in first row and second in secod row 
+'''Q4 add two string one in first row and second in secod row 
 input: 
 row1 = ross
 row2 = taylor 
@@ -67,3 +67,50 @@ def join_string(first, second):
     return print(b)
 
 
+'''Q5 Statistics challenge find mean mode and median for input list 
+input parameters - 
+Given an array, X ,N of  integers, calculate and print the respective mean, median, and mode on separate lines. 
+If your array contains more than one modal value, choose the numerically smallest one. '''
+
+N = int(input())
+a = input()
+arr = list(a.split())
+output = [eval(x) for x in arr]
+output.sort()
+import statistics
+#one method to find MEAN 
+mean = statistics.mean(output)
+print(mean)
+#another 
+mean1 = sum(output)/len(output)
+print(mean1)
+# to find Median by statistics library 
+Median = statistics.median(output)
+
+#find Median by conventional way 
+n1 = len(output)
+if n1 % 2 == 0:
+    median1 = output[n1//2]
+    median2 = output[n1//2 - 1]
+    median = (median1 + median2)/2
+else:
+    median = output[n1//2]
+print(median)
+
+# MODE
+mode = statistics.mode(output)
+print(mode)
+
+# Another method to find MODE 
+def find_mode(list):
+    counts = {}
+    if x in list:
+        counts[x] += 1 
+    else:
+        counts[x] = 1 
+    max_value = max(counts.values())
+    result = []
+    for a, count in counts.items():
+        if count == max_value:
+            result.append(a)
+    return print(min(result))
